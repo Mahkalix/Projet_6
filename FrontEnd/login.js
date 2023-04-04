731334f7dffce6e1eb65901ad12a7203bb07a3db
Maxence Badin Léger <maxencelger@gmail.com>
2023-04-04 19:46:12 +0200

    enregistrement du token dans le local storage

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,34 +1,28 @@
 const submit = document.querySelector('input[type="submit"]');
 
-const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
-
 
 submit.addEventListener("click", function (e) {
     e.preventDefault()
     FetchUserLogin()
-
 });
 
 
-const init = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "accept": "application/json",
-    },
-
-    body: JSON.stringify({
-        "email": email,
-        "password": password,
-    })
-};
 
 async function FetchUserLogin() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     try {
-        const response = await fetch("http://localhost:5678/api/users/login", init)
-        const dataUser = await response.json();
-        console.log(dataUser);
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                accept: "application/json",
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password,
+            })
+        });
 
         if (response.ok) {
             window.location.href = "./index.html";
@@ -37,11 +31,19 @@ async function FetchUserLogin() {
         else {
             alert("User not found");
         }
+
+
+        // Token d'autentification
+        const dataUser = await response.json();
+        // console.log(dataUser)
+
+        // Enregistrement du token d'authentification dans le stockage local
+        localStorage.setItem("token", dataUser.token)
+
     }
 
     catch (e) {
         console.log(e);
     }
 };
-
 
