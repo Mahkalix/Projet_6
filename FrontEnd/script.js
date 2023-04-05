52b977b2a9faf5b958b8c553abdb60615f7462fc
Maxence Badin Léger <maxencelger@gmail.com>
2023-04-05 16:59:52 +0200

    mode édition sans login
    css html et js pour la modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -166,3 +166,13 @@ function Filters() {
 // }
 
 
+// MODALE
+
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+function toggleModal(){
+  modalContainer.classList.toggle("active")
+}
